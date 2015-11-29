6a4d6c706eaab98d2e411550afc621b50d4d5b97
Kate-brekhunets <kate.doca@gmail.com>
2015-11-30 01:56:33 +0300

    Категории для перевода (класс с функциями)

---
+++ Unicon1.0/sc/source code/Unicon1.0/MeasurementFactory.java
@@ -0,0 +1,27 @@
+package sc.source code.Unicon1.0;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map;
+
+import sc.source code.Unicon1.0.units.MeasurementUnitFahrenheit;
+import sc.source code.Unicon1.0.units.MeasurementUnitKelvin;
+
+
+public class MeasurementFactory
+{
+	// категории
+	public final static String MEASURE_LENGTH       = "Length";
+	public final static String MEASURE_WEIGHT       = "Weight";
+	public final static String MEASURE_TEMPERATURE  = "Temperature";
+	public final static String MEASURE_VOLUME       = "Volume";
+	public final static String MEASURE_AREA         = "Area";
+	public final static String MEASURE_SPEED	    = "Speed";
+	public final static String MEASURE_RADIATION    = "Radiation";
+	public final static String MEASURE_ANGLE        = "Angle Measurments";
+	public final static String MEASURE_STORAGE      = "Digital Storage";
+	public final static String MEASURE_CURRENCY     = "Currency"; 
+	// м б добавить (по времени) денежную категорию 
+	}
